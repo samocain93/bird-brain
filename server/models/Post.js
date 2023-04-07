ffb9b517bb3791ad273c5209bb53f23a61ce3bcc
Sam O'Cain <samocain93@gmail.com>
2023-04-06 21:03:42 -0500

    post model added

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,3 +1,36 @@
 const mongoose = require('mongoose');
 
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    location: String,
+    description: String,
+    picturePath: String,
+    userPicturePath: String,
+    likes: {
+      type: Number,
+      default: 0,
+    },
+    comments: {
+      type: Array,
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
+const Post = mongoose.model('Post', postSchema);
+export default Post;
