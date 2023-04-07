08aa0575b526df06bd36f1249fbc2153bf2de3f0
Sam O'Cain <samocain93@gmail.com>
2023-04-06 19:42:08 -0500

    new folder structure updated

--- server/index.js
+++ server/index.js
@@ -1,50 +1,22 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
-import multer from "multer";
-import helmet from "helmet";
-import morgan from "morgan";
-import path from "path";
-import { fileURLToPath } from "url";
+const express = require('express');
+const colors = require('colors');
+require('dotenv').config();
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+const connectDB = require('./config/db');
+const port = process.env.PORT || 3000;
 
-/*CONFIGURATIONS */
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-dotenv.config();
 const app = express();
-app.use(express.json());
-app.use(helmet());
-app.use(helmet.crossOriginResouracePolicy({ policy: "cross-origin"}));
-app.use(morgan("common"));
-app.use(bodyParser.json({limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
-app.use(cors());
-app.use("./assets", express.static(path.join(__dirname, "public/assets")));
 
-// file storage
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'public/assets');
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname);
-    },
-  });
-  
-  const upload = multer({ storage });
-  
-  // mongoose setup
-  const PORT = process.env.PORT || 6001;
-  mongoose
-    .connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
-    })
-    .catch((error) => console.log(error.message));
+// connect do DB
+connectDB();
 
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development',
+  })
+);
+
+app.listen(port, console.log(`Server running on port ${port}`));
