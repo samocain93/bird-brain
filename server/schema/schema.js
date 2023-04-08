ab0c5b677533f295032fcaf50cb98ae0193b03a1
Sam O'Cain <samocain93@gmail.com>
2023-04-07 23:35:02 -0500

    pausing. continuing schema setup tomorrow

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,2 +1,20 @@
 const { users, posts } = require('../sampleData');
 
+const User = require('../models/User');
+const Post = require('../models/Post');
+
+// Bring in graphql types
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLId,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLEnumType,
+  GraphQLInt,
+  GraphQLBoolean,
+} = require('graphql');
+
+// Create types
+
---
+++ server/testData.js
@@ -0,0 +1,5 @@
+const users = [{}];
+
+const posts = [{}];
+
+module.exports = { users, posts };
