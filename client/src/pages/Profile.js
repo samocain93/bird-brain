a5e23daa14664ed95f5587fcf65cdbe02b3ee7cf
marchetype <114883447+marchetype@users.noreply.github.com>
2023-04-10 13:55:16 -0400

    profile avatar is set up. Theme/style color palette options are added.

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,30 +1,37 @@
-import { Container, Box } from "@mui/material";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-
-const prof_theme = createTheme({
-  palette: {
-    primary: {
-      main: "#FDB813",
-    },
-    secondary: {
-      main: "#FF8F00",
-    },
-  }
-})
+import { Container, Box, Avatar } from "@mui/material";
+import { useTheme, ThemeProvider } from "@mui/material/styles";
 
 
 
 function Profile() {
+  const prof_theme = useTheme();
   return (
     <ThemeProvider theme={prof_theme}>
       <Container>
         <Box
           sx={{
-            width: 600,
+            width: 1000,
             height: 300,
             backgroundColor: 'primary.main',
           }}
+        >
+        <Box
+          sx={{
+            width: 20,
+            height: 10,
+          }}
         />
+        <Avatar
+          sx={{
+            width: 150,
+            height: 150,
+            mt: 8,
+            marginLeft: 10,
+            fontSize: 40,
+            backgroundColor: 'profile_avatar.main'
+          }}>MB</Avatar>
+          
+        </Box>
         <h1>This is the Profile Page</h1>
       </Container>
     </ThemeProvider>
