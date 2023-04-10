b2ec340c65c55c8b2a9c08712e821462e657d322
marchetype <114883447+marchetype@users.noreply.github.com>
2023-04-10 13:18:25 -0400

    Starting work on profile page.

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,10 +1,34 @@
-import { Container } from "@mui/material";
+import { Container, Box } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const prof_theme = createTheme({
+  palette: {
+    primary: {
+      main: "#FDB813",
+    },
+    secondary: {
+      main: "#FF8F00",
+    },
+  }
+})
+
+
 
 function Profile() {
   return (
-    <Container>
-      <h1>This is the Profile Page</h1>
-    </Container>
+    <ThemeProvider theme={prof_theme}>
+      <Container>
+        <Box
+          sx={{
+            width: 600,
+            height: 300,
+            backgroundColor: 'primary.main',
+          }}
+        />
+        <h1>This is the Profile Page</h1>
+      </Container>
+    </ThemeProvider>
+    
   );
 }
 
