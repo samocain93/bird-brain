69ad38ed1533aa7661ff47af1ddf28d34bef3d63
marchetype <114883447+marchetype@users.noreply.github.com>
2023-04-10 14:21:04 -0400

    Adding sections for profile text to display. will place grid below profile header with all of user's posts.

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -10,29 +10,43 @@ function Profile() {
       <Container>
         <Box
           sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-around',
             width: 1000,
-            height: 300,
+            height: 200,
             backgroundColor: 'primary.main',
+            borderBottomLeftRadius: 20,
+            borderBottomRightRadius: 20
           }}
         >
-        <Box
-          sx={{
-            width: 20,
-            height: 10,
-          }}
-        />
         <Avatar
           sx={{
-            width: 150,
-            height: 150,
-            mt: 8,
-            marginLeft: 10,
+            width: 100,
+            height: 100,
+            marginLeft: 0,
             fontSize: 40,
             backgroundColor: 'profile_avatar.main'
-          }}>MB</Avatar>
-          
+            }}
+            >MB</Avatar>
+        <Box
+          sx={{
+            border: 2,
+            borderRadius: 20,
+            padding: 3,
+            width: 600,
+            height: 175,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'space-around',
+          }}
+          id="bio-content"
+        >
+          <h3> Heres the Name/Username Display </h3>
+          <p>Here's the Bio Display</p>
         </Box>
-        <h1>This is the Profile Page</h1>
+        </Box>
+        
       </Container>
     </ThemeProvider>
     
