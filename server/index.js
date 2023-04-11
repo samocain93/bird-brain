4793a3024eaaacb9b819595cb2def333852e18c1
cxlos <bautistac_29@outlook.com>
2023-04-11 14:27:05 -0500

    committing just to pull

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const colors = require('colors');
+// const colors = require('colors');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '../.env') });
@@ -17,26 +17,26 @@ const app = express();
 // connect do DB
 // connectDB();
 
-// const startApolloServer = async () => {
-//   await server.start();
-//   server.applyMiddleware({ app });
+const startApolloServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
 
-//     db.once('open', () => {
-//     app.listen(PORT, () => {
-//       console.log(`API server running on port ${PORT}!`);
-//       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-//     })
-//   })
-// };
-
-// startApolloServer(typeDefs, resolvers);
-
-app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema,
-    graphiql: true,
+    db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
   })
-);
+};
+
+startApolloServer(typeDefs, resolvers);
+
+// app.use(
+//   '/graphql',
+//   graphqlHTTP({
+//     schema,
+//     graphiql: true,
+//   })
+// );
 
 app.listen(port, console.log(`Server running on port ${port}`));
