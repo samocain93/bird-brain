ae2737a2f83f95de695ea4e525890b03e23c9d01
marchetype <114883447+marchetype@users.noreply.github.com>
2023-04-10 20:50:52 -0400

    Created rough profile page layout with post grid below profile header. Planning on adding further stylization and responsiveness to design.

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,4 +1,4 @@
-import { Container, Box, Avatar } from "@mui/material";
+import { Container, Box, Avatar, Grid, Paper} from "@mui/material";
 import { useTheme, ThemeProvider } from "@mui/material/styles";
 
 
@@ -6,20 +6,22 @@ import { useTheme, ThemeProvider } from "@mui/material/styles";
 function Profile() {
   const prof_theme = useTheme();
   return (
-    <ThemeProvider theme={prof_theme}>
-      <Container>
-        <Box
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-around',
-            width: 1000,
-            height: 200,
-            backgroundColor: 'primary.main',
-            borderBottomLeftRadius: 20,
-            borderBottomRightRadius: 20
+  <ThemeProvider theme={prof_theme}>
+    <Container>
+      <Box
+        sx={{
+          width: 1000,
+          height: 200,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-around',
+          backgroundColor: 'primary.main',
+          borderBottomLeftRadius: 20,
+          borderBottomRightRadius: 20,
+          boxShadowTop: 0,
+          boxShadowBottom: 3,
           }}
-        >
+      >
         <Avatar
           sx={{
             width: 100,
@@ -27,8 +29,8 @@ function Profile() {
             marginLeft: 0,
             fontSize: 40,
             backgroundColor: 'profile_avatar.main'
-            }}
-            >MB</Avatar>
+          }}
+        >MB</Avatar> {/* CONTENT DISPLAY */}
         <Box
           sx={{
             border: 2,
@@ -39,14 +41,35 @@ function Profile() {
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'space-around',
-          }}
+          }}  
           id="bio-content"
         >
-          <h3> Heres the Name/Username Display </h3>
-          <p>Here's the Bio Display</p>
+          <h3> Heres the Name/Username Display </h3> {/* CONTENT DISPLAY */}
+          <p>Here's the Bio Display</p> {/* CONTENT DISPLAY */}
         </Box>
-        </Box>
-        {/*will add grid here to display user's posts. */}
+      </Box>
+
+      <Grid container>
+        {/* function below should map all of user's posts to grid once backend is connected, 
+        for now a sample grid is provided.*/}
+        {/* {posts.map(post => (
+          <Grid item key={post.id}>
+            <Paper>{ post.content }</Paper>
+          </Grid>
+        ))} */}
+        <Grid item xs={12} md={6}>
+          <Paper>Post 1</Paper>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Paper>Post 2</Paper>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Paper>Post 3</Paper>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Paper>Post 4</Paper>
+        </Grid>
+      </Grid>
       </Container>
     </ThemeProvider>
     
