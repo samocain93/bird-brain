ee9e0f58af80349495684022399c71be80eafeb2
Sam O'Cain <samocain93@gmail.com>
2023-04-11 21:01:27 -0500

    pushing up resolvers

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -20,17 +20,24 @@ const resolvers = {
 
     // return a single user
     user: async (parent, args) => {
-      return User.findById(args.id).populate('posts');
+      return User.findOne(args.id)
     },
 
     // return a single post
     post: async (parent, args) => {
-      return Post.findById(args.id).populate('comments').populate('user');
+      return Post.findById(args.id).populate({
+        path: 'comments',
+        populate: { path: 'user', model: User },
+      });
     },
 
     // return all comments
     comments: async () => {
-      return Comment.find({}).populate('user').populate('post');
+      return Comment.find({}).populate({
+        path: 'user',
+        populate: { path: 'post', model: Post },
+      });
+
     },
 
     // return a single comment
