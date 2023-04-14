74ca315b4b64e1a521313f1d35d1fb95963d73ee
Mike B <115735557+mbartnett@users.noreply.github.com>
2023-04-13 20:10:20 -0500

    pushing more JWT work

--- server/models/User.js
+++ server/models/User.js
@@ -26,5 +26,20 @@ const userSchema = new Schema({
   ],
 });
 
+// set up pre-save middleware to create password
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
 const User = model('User', userSchema);
 module.exports = User;
--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -54,9 +54,33 @@ const resolvers = {
 
   Mutation: {
     // add a user
-    addUser: async (parent, args) => {
-      const user = await User.create(args);
-      return user;
+    addUser: async (parent, { name, email, password }) => {
+      const user = await User.create({ name, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      // Look up ther user by the provided email address. Since the `email` field is unique, we know that only one person will exist with that email.
+      const user = await User.findOne({ email });
+
+      // If there is no user with that email address, return an Authentication error stating so
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      // If there is a user found, execute the `isCorrectPassword` instance method and check if the correct password was provided
+      const correctPw = await user.isCorrectPassword(password);
+
+      // If the password is incorrect, return an Authentication error stating so
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      // If email and password are correct, sign the token and return it to the client
+      const token = signToken(user);
+
+      // Return an 'Auth' object that consists of the signed token and user's information
+      return { token, user };
     },
 
     // add a post
