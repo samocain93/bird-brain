882322b550f75aaac87a7aba2ff98a68c983c153
cxlos <bautistac_29@outlook.com>
2023-04-14 19:45:33 -0500

    fixed issue with app not changing state to loggedIn: true after signing in, and also added logout button

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,50 +4,31 @@ const secret = 'keepitsecretkeepitsafe';
 const expiration = '1h';
 
 module.exports = {
+
+    authMiddleware: function ({ req }) {
+        let token = req.body.token || req.query.token || req.headers.authorization;
+    
+        if (req.headers.authorization) {
+          token = token.split(' ').pop().trim();
+        }
+    
+        if (!token) {
+          return req;
+        }
+    
+        try {
+          const { data } = jwt.verify(token, secret, { maxAge: expiration });
+          req.user = data;
+          console.log('req.user: ', req.user);
+        } catch {
+          console.log('Invalid token');
+        }
+    
+        return req;
+      },
+
     signToken: function ({ name, email, _id }) {
         const payload = { name, email, _id };
-
         return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
     },
 }
-
-// Carlos
-/* 
-import decode from 'jwt-decode';
-
-
-class AuthService {
-    getProfile() {
-        return decode(this.getToken());
-    }
-
-    loggedIn() {
-        const token = this.getToken();
-        return token && !this.isTokenExpired(token) ? true : false;
-    }
-
-    isTokenExpired(token) {
-        const decoded = decode(token);
-        if (decoded.exp < Date.now() / 1000) {
-            localStorage.removeItem('id_token');
-            return true;
-        }
-        return false;
-    }
-
-    getToken() {
-        return localStorage.getItem('id_token');
-    }
-
-    login(idToken) {
-        localStorage.setItem('id_token', idToken);
-        window.location.assign('/');
-    }
-
-    logout() {
-        localStorage.removeItem('id_token');
-        window.location.reload();
-    }
-}
-
-export default new AuthService(); */
